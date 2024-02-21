926186a1b5e5dca0c3985b478ed14c3bfca5e8cd
greeshmaps1992@gmail.com <greeshmaps1992@gmail.com>
2024-02-21 12:59:46 +0530

    frontpage

--- pjctmain/settings.py
+++ pjctmain/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pjctmain.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'pjctmain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'review',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',    # Set to your database host if not running locally
+        'PORT': '5432',         # Set to your database port if different
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -116,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT=BASE_DIR /'static'
+STATICFILES_DIRS=[
+    'pjctmain/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
